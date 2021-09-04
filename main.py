47540088076dc71053f565ff9822c2ec9c1449ae
Patryk Klimek <patryk.klimek@scalac.io>
2021-09-04 15:04:16 +0200

    Added working selenium chrome driver

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def selenium_simulation():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.barnesandnoble.com/w/eye-of-the-world-robert-jordan/1100356869?ean=9781250768681')
 
 
 def main():
-    print('hi')
+    selenium_simulation()
 
 
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
