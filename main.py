d7b6182ceb5d74bc9b00add9104e54da4c91e9af
Nomuron <Nomuron@gmail.com>
2021-09-04 16:29:23 +0200

    Made generator of webscrapped opinions

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def selenium_simulation():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://www.barnesandnoble.com/w/eye-of-the-world-robert-jordan/1100356869?ean=9781250768681')
+    opinion_parent = driver.find_elements_by_class_name("bv-content-summary-body-text")
+
+    opinions = [opinion.find_element_by_tag_name("p").text for opinion in opinion_parent]
+    yield opinions
 
 
 def main():
-    selenium_simulation()
+    for text in selenium_simulation():
+        print(text)
 
 
 if __name__ == '__main__':
