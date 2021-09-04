00f25b71c0357c32ffb242e653e80787cfbe9824
Nomuron <Nomuron@gmail.com>
2021-09-04 16:41:21 +0200

    Optimized CSS selector for finding elements

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def selenium_simulation():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://www.barnesandnoble.com/w/eye-of-the-world-robert-jordan/1100356869?ean=9781250768681')
-    opinion_parent = driver.find_elements_by_class_name("bv-content-summary-body-text")
-
-    opinions = [opinion.find_element_by_tag_name("p").text for opinion in opinion_parent]
+    opinion_parent = driver.find_elements_by_css_selector('.bv-content-summary-body-text p')
+    opinions = [opinion.text for opinion in opinion_parent]
+    # opinions = [opinion.find_element_by_tag_name("p").text for opinion in opinion_parent]
     yield opinions
 
 
