226a63838d7eab8de9d3cbaa28ee96b82fe29e63
Nomuron <Nomuron@gmail.com>
2021-09-06 21:07:56 +0200

    Made opinion rating scrapping code. Started building generator for dictionary opinion:sentiment, tbd

--- main.py
+++ main.py
@@ -3,16 +3,39 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 URL = 'https://www.barnesandnoble.com/w/eye-of-the-world-robert-jordan/1100356869?ean=9781250768681'
+OPINION_CSS_SELECTOR = '.bv-content-summary-body-text:nth-child(1)'
+RATE_CSS_SELECTOR = '.bv-rating-stars-container .bv-off-screen'
+
+
+def opinion_rating_compositor(opinion_review: list, opinion_rate: list) -> dict:
+    sentiment_rates = []
+
+    for opinion in opinion_review:
+        for rate in opinion_rate:
+            if rate >= 4:
+                sentiment_rates.append('positive')
+            elif rate <= 2:
+                sentiment_rates.append('negative')
+            else:
+                continue
+
+
+
+
 
 
 def selenium_simulation():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(URL)
-    opinion_parent = driver.find_elements_by_css_selector('.bv-content-summary-body-text:nth-child(1)')
-    opinions = [opinion.text for opinion in opinion_parent]
+
+    opinion_web_elements = driver.find_elements_by_css_selector(OPINION_CSS_SELECTOR)
+    opinions = [opinion.text for opinion in opinion_web_elements]
+
+    rates_web_elements = driver.find_elements_by_css_selector(RATE_CSS_SELECTOR)
+    rates = [rate.text[0] for rate in rates_web_elements]
 
     for opinion in opinions:
-        yield opinion
+        yield {opinion: rates}
 
 
 def main():
